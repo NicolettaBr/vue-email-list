11a5ffe5adf132a3c270459b72ab0108784fd762
Nicoletta Berrettoni <nico.berrettoni@gmail.com>
2021-04-27 16:48:42 +0200

    email stampate

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ var app = new Vue(
     {
         el: '#root',
         data: {
-            arrayMail:[]
+            arrayMail: []
+        
         },
 
         method: {
@@ -10,25 +11,23 @@ var app = new Vue(
         },
 
         mounted(){
-            axios
+            for(let i = 0; i < 10; i++) {
+                axios
                 .get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then((response)=>{
                     //console.log(response);
                     
                     const result = response.data;
                     //console.log(result);
+
                     const mail = result.response;
-                    //console.log(mail);
+                    console.log(mail);
 
-                    for(var i = 0; i < 10; i++){
+                    this.arrayMail.push(mail);
                     
-                        const mail = result.response;
-                        console.log(mail);
-                        console.log(i);
-                    }
-                
-
                 });
+            }
+            
         }
     }
 )
